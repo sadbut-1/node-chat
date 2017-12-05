c5173ffc2bffd8dd6857c5049afd984fd09e36fd
sadbut_1 <eduardommoya@gmail.com>
2017-12-05 12:07:06 -0200

    first commit

---
+++ src/configs/env/development.js
@@ -0,0 +1,9 @@
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const expressSession = require('express-session')
+const bodyParser = require('body-parser')
+const hbs = require('express-hbs')
+
+module.exports = (app) => {
+
+}
